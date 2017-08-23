d0037d0e69bd3f91d4cd68b54849f3bd4426d1f7
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-08-23 17:42:11 +0700

    Initial commit

---
+++ Source/ISO9075Encoder.Tests/EncoderTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace ISO9075Encoder.Tests
+{
+    public class EncoderTests
+    {
+        [Fact]
+        public void Encode()
+        {
+            var text = "Hello & World 123";
+            var expect = "Hello_x0020__x0026__x0020_World_x0020_123";
+            Assert.Equal(expect, Encoder.Encode(text));
+        }
+
+        [Fact]
+        public void Decode() {
+            var encoded = "Hello_x0020__x0026__x0020_World_x0020_123";
+            var text = "Hello & World 123";
+            Assert.Equal(text, Encoder.Decode(encoded));
+        }
+    }
+}
---
+++ Source/ISO9075Encoder/Encoder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ISO9075Encoder
+{
+    public class Encoder
+    {
+        public static string Encode(string source) => System.Xml.XmlConvert.EncodeName(source);
+        public static string Decode(string iso9075String) => System.Xml.XmlConvert.DecodeName(iso9075String);
+    }
+}
